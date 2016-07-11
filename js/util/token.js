68da798f6cf3afe7fb2e77a1cdec8ab0f2243d66
Minh Nguyễn <mxn@1ec5.org>
2016-07-10 17:47:09 -0700

    Structured token substitution
    Introduced an alternative to the token substitution mini-language that uses structured JSON syntax to specify substitutions and recursive defaults.

--- js/util/token.js
+++ js/util/token.js
@@ -6,12 +6,33 @@ module.exports = resolveTokens;
  * Replace tokens in a string template with values in an object
  *
  * @param {Object} properties a key/value relationship between tokens and replacements
- * @param {string} text the template string
+ * @param {string|Object} value a template string or substitution function object
  * @returns {string} the template with tokens replaced
  * @private
  */
-function resolveTokens(properties, text) {
-    return text.replace(/{([^{}]+)}/g, function(match, key) {
-        return key in properties ? properties[key] : '';
-    });
+function resolveTokens(properties, value) {
+    if (typeof value === 'string') {
+        return value.replace(/{([^{}]+)}/g, function(match, ref) {
+            return ref in properties ? properties[ref] : '';
+        });
+    } else if (typeof value === 'object' && value.type === 'substitution') {
+        var components = value.value;
+        var text = '';
+        for (var i = 0; i < components.length; i++) {
+            var component = components[i];
+            if (typeof component === 'string') {
+                text += component;
+            } else if (component.ref in properties) {
+                text += properties[component.ref];
+            } else if ('default' in component) {
+                text += resolveTokens(properties, {
+                    type: 'substitution',
+                    value: [component.default]
+                });
+            }
+        }
+        return text;
+    } else {
+        return '';
+    }
 }
--- test/js/util/token.test.js
+++ test/js/util/token.test.js
@@ -4,6 +4,7 @@ var test = require('tap').test;
 var resolveTokens = require('../../../js/util/token');
 
 test('token', function(t) {
+    t.equal('', resolveTokens({name:'14th St NW'}, ''));
     t.equal('literal', resolveTokens({name:'14th St NW'}, 'literal'));
     t.equal('14th St NW', resolveTokens({name:'14th St NW'}, '{name}'));
     t.equal('', resolveTokens({text:'14th St NW'}, '{name}'));
@@ -14,6 +15,51 @@ test('token', function(t) {
     t.equal('dashed', resolveTokens({'dashed-property': 'dashed'}, '{dashed-property}'));
     t.equal('150 m', resolveTokens({'HØYDE': 150}, '{HØYDE} m'));
     t.equal('mapbox', resolveTokens({'$special:characters;': 'mapbox'}, '{$special:characters;}'));
+
+    t.equal('', resolveTokens({name:'Louisiana'}, {
+        type: 'substitution',
+        value: []
+    }));
+    t.equal('abc', resolveTokens({name:'Louisiana'}, {
+        type: 'substitution',
+        value: ['abc']
+    }));
+    t.equal('abc', resolveTokens({name:'Louisiana'}, {
+        type: 'substitution',
+        value: ['a', 'b', 'c']
+    }));
+    t.equal('“”', resolveTokens({name:''}, {
+        type: 'substitution',
+        value: ['“', {ref: 'name'}, '”']
+    }));
+    t.equal('“Louisiana”', resolveTokens({name: 'Louisiana'}, {
+        type: 'substitution',
+        value: ['“', {ref: 'name'}, '”']
+    }));
+    t.equal('“?”', resolveTokens({name: 'Louisiana', 'name_fr': 'Louisiane'}, {
+        type: 'substitution',
+        value: ['“', {ref: 'name_en', default: '?'}, '”']
+    }));
+    t.equal('“Louisiana”', resolveTokens({name: 'Louisiana', 'name_fr': 'Louisiane'}, {
+        type: 'substitution',
+        value: ['“', {ref: 'name_en', default: {ref: 'name'}}, '”']
+    }));
+    t.equal('“Louisiane”', resolveTokens({name: 'Louisiana', 'name_fr': 'Louisiane'}, {
+        type: 'substitution',
+        value: ['“', {ref: 'name_fr', default: {ref: 'name'}}, '”']
+    }));
+    t.equal('Louisiana (Louisiane)', resolveTokens({name: 'Louisiana', 'name_fr': 'Louisiane'}, {
+        type: 'substitution',
+        value: [{ref: 'name'}, ' (', {ref: 'name_lou', default: {ref: 'name_fr', default: '⚜'}}, ')']
+    }));
+    t.equal('Louisiana ()', resolveTokens({name: 'Louisiana', 'name_fr': 'Louisiane'}, {
+        type: 'substitution',
+        value: [{ref: 'name'}, ' (', {ref: '', default: {ref: 'name_lou'}}, ')']
+    }));
+    t.equal('Louisiana (Louisiane)', resolveTokens({name: 'Louisiana', '': 'Louisiane'}, {
+        type: 'substitution',
+        value: [{ref: 'name'}, ' (', {ref: '', default: {ref: 'name_fr', default: '⚜'}}, ')']
+    }));
 
     t.end();
 });
